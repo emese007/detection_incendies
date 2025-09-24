dfd8f28923b2ef8ae86162fe7974282d0826fc3a
17matb <147427088+17matb@users.noreply.github.com>
2025-09-24 16:49:53 +0200

    feat: provide function to copy image files along with coco conversion

--- prepare_data/data_preparation.py
+++ prepare_data/data_preparation.py
@@ -1,7 +1,19 @@
 from pathlib import Path
+import shutil
 from ultralytics.data.converter import convert_coco
 
 
+def copy_images(images_path):
+    source_directory = Path(images_path)
+    destination_directory = source_directory.parent.parent / 'converted' / 'images'
+
+    for item in source_directory.glob('*.jpg'):
+        shutil.copy(item, destination_directory / item.name)
+    print('· Succesfully copied all images.')
+
+
 def convert_annotations(coco_json_path):
-    output = Path(coco_json_path).parent.parent / 'converted'
-    convert_coco(coco_json_path, str(output))
+    destination_directory = Path(coco_json_path).parent.parent / 'converted'
+    if not destination_directory.is_dir():
+        convert_coco(coco_json_path, str(destination_directory))
+        copy_images(coco_json_path)
